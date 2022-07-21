3cddebf98819d26b37e601c5d19ceb6548a4d25d
Pascal Garber <pascal@artandcode.studio>
2022-07-21 13:19:44 +0200

    use latest version in deno example

--- examples/deno/index.ts
+++ examples/deno/index.ts
@@ -1,3 +1,3 @@
 #! /usr/bin/env -S deno run
-import dirname from 'https://deno.land/x/cross_dirname@v0.0.4/mod.ts';
-console.log(dirname()); // outputs "/path/to/the"
+import dirname from 'https://deno.land/x/cross_dirname/mod.ts';
+console.log(dirname());
